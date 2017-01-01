c25e1f5a1d2e8356d9db2ba372441281af753fd4
Chad Sheets <cjsheets@gmail.com>
2017-01-01 14:13:38 -0800

    added boilerplate

---
+++ rotate-wallpapers/getIdle.c
@@ -0,0 +1,18 @@
+#include <X11/extensions/scrnsaver.h>
+#include <stdio.h>
+
+int main(void) {
+    Display *dpy = XOpenDisplay(NULL);
+
+    if (!dpy) {
+        return(1);
+    }
+
+    XScreenSaverInfo *info = XScreenSaverAllocInfo();
+    XScreenSaverQueryInfo(dpy, DefaultRootWindow(dpy), info);
+    printf("%u\n", info->idle);
+
+      return(0);
+}
+
+/* gcc -o getIdle getIdle.c -lXss -lX11 */
