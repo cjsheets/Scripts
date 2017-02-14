1ece80d17c65893adfd268fd6fe044e6f1c9abfe
Chad Sheets <cjsheets@gmail.com>
2017-02-13 19:26:45 -0800

    fixed helper scripts

--- conky/gcalcli_agenda.sh
+++ conky/gcalcli_agenda.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 gcalcli --conky --calendar 'Work' --calendar 'Personal' --calendar 'Family' \
     agenda $(date +%m/%d/%Y) $(date +%m/%d/%Y -d "+3 days") |
-    sed -e 's/yellow/white/g' \
-        -e 's/cyan/#0077ff/g' \
+    sed -e 's/white/#0077ff/g' \
+        -e 's/yellow/#0077ff/g' \
+        -e 's/cyan/white/g' \
         -e '/$2500/ d'
 
 # $2500 processed out a spam message
--- conky/gcalcli_week.sh
+++ conky/gcalcli_week.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 gcalcli --conky --calendar 'Work' --calendar 'Personal' --nolineart -w 18 calw 1 |
-    sed -e 's/yellow/white/g' \
-        -e 's/cyan/#0077ff/g'
+    sed -e 's/white/#0077ff/g' \
+        -e 's/yellow/#0077ff/g' \
+        -e 's/cyan/white/g'
